d65ad3bb1c1a75a4cf4f139533a5037cdd613bbd
MrLeeYongSheng <731153234@qq.com>
2018-01-09 14:34:59 +0800

    1.集成了TOPJUI,其中index.html移到了/WEB-INF/views/下 2.去除了WebConfig的ResourceHandler的配置,由于WebApp/下的非/WEB-INF/下的内容均可以作为 静态内容处理(配置了DefaultServletHandling)

--- src/main/java/com/lys/zhku/config/WebConfig.java
+++ src/main/java/com/lys/zhku/config/WebConfig.java
@@ -19,18 +19,22 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 	public void configureViewResolvers(ViewResolverRegistry registry) {
 		// 注册ViewResolvers
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
-		resolver.setViewClass(JstlView.class);// 支持JStL模板
+		//InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setViewClass(JstlView.class);// 支持JSTL模板
 		registry.viewResolver(resolver);
 	}
 
 	@Override
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 		configurer.enable();// 让Servlet容器默认的Servlet处理静态资源的请求,而不是使用DispatcherServlet来处理
+							// 让webapp/文件夹下的非/WEB-INF/文件夹的容可得到处理
+							//DispatcherServlet无法处理的url交给默认的Servlet处理
 	}
-
+/*
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	//用来映射静态资源,可映射/WEB-INF/文件夹的内容
+		registry.addResourceHandler("/resources/**").addResourceLocations("/WEB-INF/views/");
 	}
-
+*/
 }
